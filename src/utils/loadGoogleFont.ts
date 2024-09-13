bcc6093d507c5bbe1e5ed8f529f08262f9369a4e
hushin <ngtv.hsn@gmail.com>
2024-09-13 20:35:22 +0900

    Update Google Fonts configuration

--- src/utils/loadGoogleFont.ts
+++ src/utils/loadGoogleFont.ts
@@ -45,14 +45,14 @@ async function loadGoogleFonts(
 > {
   const fontsConfig = [
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono",
+      name: "BIZ UDPGothic",
+      font: "BIZ+UDPGothic",
       weight: 400,
       style: "normal",
     },
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono:wght@700",
+      name: "BIZ UDPGothic",
+      font: "BIZ+UDPGothic:wght@700",
       weight: 700,
       style: "bold",
     },
