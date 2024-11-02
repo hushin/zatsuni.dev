2a843064c05ef099479ac820b3de3ee98aa89160
hushin <ngtv.hsn@gmail.com>
2024-11-02 09:41:45 +0900

    titleを雑にdevに修正

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SITE: Site = {
   author: "hush_in",
   profile: "https://zatsuni.dev/",
   desc: "雑になんか作る。雑記も書く。",
-  title: "zatsuni.dev",
+  title: "雑にdev",
   ogImage: "zatsuni-og.png",
   lightAndDarkMode: true,
   postPerIndex: 4,
