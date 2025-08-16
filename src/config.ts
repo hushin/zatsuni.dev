103b75c361f97398199592c78fe802c9ae8c09e8
hushin <ngtv.hsn@gmail.com>
2025-08-16 21:10:12 +0900

    feat: SITEとLOCALEの設定を追加し、ソーシャルリンクの型を定義

---
+++ src/config.ts
@@ -0,0 +1,77 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://zatsuni.dev/", // replace this with your deployed domain
+  author: "hush_in",
+  profile: "https://zatsuni.dev/",
+  desc: "雑になんか作る。雑記も書く。",
+  title: "雑にdev",
+  // ogImage: "zatsuni-og.png",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 10,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+};
+
+export const LOCALE = {
+  lang: "ja", // html lang code. Set this empty and default will be "en"
+  langTag: ["ja-JP"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "X",
+    href: "https://x.com/hush_in",
+    linkTitle: `X(Twitter)`,
+    active: true,
+  },
+  {
+    name: "Bluesky",
+    href: "https://bsky.app/profile/hushin.bsky.social",
+    linkTitle: `Bluesky`,
+    active: true,
+  },
+  {
+    name: "Github",
+    href: "https://github.com/hushin",
+    linkTitle: `Github`,
+    active: true,
+  },
+  {
+    name: "Zenn",
+    href: "https://zenn.dev/hush_in",
+    linkTitle: `Zenn`,
+    active: true,
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/hush_in",
+    linkTitle: `Instagram`,
+    active: true,
+  },
+  {
+    name: "YouTube",
+    href: "https://www.youtube.com/@hush_in",
+    linkTitle: `YouTube`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:ngtv.hsn@gmail.com",
+    linkTitle: `Send an email to ${SITE.author}`,
+    active: true,
+  },
+  {
+    name: "Discord",
+    href: "https://discordapp.com/users/336255981111738371",
+    linkTitle: `Discord`,
+    active: true,
+  },
+];
